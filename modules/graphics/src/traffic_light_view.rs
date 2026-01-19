2b67fee7099d1b8ebc3595420735a412c2ed2545
TheAccis <accis9683@gmail.com>
2026-01-20 01:39:23 +0300

    fix traffic lights

--- modules/graphics/src/traffic_light_view.rs
+++ modules/graphics/src/traffic_light_view.rs
@@ -1,14 +1,24 @@
 use macroquad::prelude::*;
-use types::*;
+use types::TrafficLight;
+use glam::Vec2;
 
 pub struct TrafficLightView {
-    pub light: TrafficLight,
-    pub dir: Direction,
+    pub traffic_light: TrafficLight,
+    pub pos: Vec2,
+    pub radius: f32,
 }
 
 impl TrafficLightView {
+    pub fn new(traffic_light: TrafficLight, pos: Vec2) -> Self {
+        Self {
+            traffic_light,
+            pos,
+            radius: 10.0,
+        }
+    }
+
     pub fn draw(&self) {
-        let color = if self.light.green { GREEN } else { RED };
-        draw_circle(self.light.pos.x, self.light.pos.y, 10.0, color);
+        let color = if self.traffic_light.green { GREEN } else { RED };
+        draw_circle(self.pos.x, self.pos.y, self.radius, color);
     }
 }
--- modules/types/src/traffic_light.rs
+++ modules/types/src/traffic_light.rs
@@ -1,13 +1,22 @@
-use glam::Vec2;
+use crate::Direction;
 
 pub struct TrafficLight {
 	pub green_duration: f32,
+    pub dir: Direction,
 	pub green: bool,
 	pub timer: f32,
-	pub pos: Vec2,
 }
 
 impl TrafficLight {
+    pub fn new(dir: Direction, green_duration: f32) -> Self {
+        Self {
+            dir,
+            green: false,
+            timer: 0.0,
+            green_duration,
+        }
+    }
+
 	pub fn update(&mut self, dt: f32) {
 		self.timer += dt;
 		if self.timer >= self.green_duration {
