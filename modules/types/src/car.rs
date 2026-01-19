df9cc4263eb89c0872949e06c3080c11638d073d
TheAccis <accis9683@gmail.com>
2026-01-20 00:08:57 +0300

    Extract magic numbers to contants

--- modules/types/src/car.rs
+++ modules/types/src/car.rs
@@ -8,6 +8,10 @@ pub struct Car {
 }
 
 impl Car {
+    pub const SPAWN_MARGIN: f32 = 50.0;
+    pub const WIDTH: f32 = 10.0;
+    pub const HEIGHT: f32 = 20.0;
+
     pub fn new(pos: Vec2, dir: Direction, speed: f32) -> Self {
         let velocity = match dir {
             Direction::North => Vec2::new(0.0, -speed),
@@ -25,8 +29,8 @@ impl Car {
     }
 
     pub fn size(&self) -> Vec2 {
-        const VERTICAL: Vec2 = Vec2::new(10.0, 20.0);
-        const HORIZONTAL: Vec2 = Vec2::new(20.0, 10.0);
+        const VERTICAL: Vec2 = Vec2::new(Car::WIDTH, Car::HEIGHT);
+        const HORIZONTAL: Vec2 = Vec2::new(Car::HEIGHT, Car::WIDTH);
 
         match self.dir {
             Direction::North | Direction::South => VERTICAL,
@@ -35,8 +39,8 @@ impl Car {
     }
 
     pub fn is_outside(&self, screen_size: Vec2) -> bool {
-        let min = Vec2::splat(-50.0);
-        let max = screen_size + 50.0;
+        let min = Vec2::splat(-Self::SPAWN_MARGIN);
+        let max = screen_size + Self::SPAWN_MARGIN;
 
         self.pos.cmpgt(max).any() || self.pos.cmplt(min).any()
     }
